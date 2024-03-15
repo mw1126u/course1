9e128ee1eed54fb614181ca00faf4362635ba968
mw1126u <161033372+mw1126u@users.noreply.github.com>
2024-03-15 13:28:15 +0000

    Think again

--- TreeSort.java
+++ TreeSort.java
@@ -13,5 +13,35 @@ private class Node {
     this.right_child = null;
   }
 }
-//Root node of the tree
+                      //Root node of the tree
+  private Node head; // Points to the head of the linked list
+
+  // constructor to initialize the linked list
+  public SinglyLinkedList() {
+    this.head = null;
+  }
+
+  //Method to add children to the tree
+
+  public void addChild(int data) {
+    Node newNode = new Node(data);
+
+    //if the list is empty, make the new node the head
+
+    if (head == null) {
+      head = newNode;
+      return;
+
+    for (i = 0; i < data.length; i++){
+      array1[i] = head
+    }
+    }
+  }
+        String temp = array1[i];
+        array1[i] = array1[i/2];
+        array1[i/2] = temp;
+        i = i/2;
+    }
+
+
   
